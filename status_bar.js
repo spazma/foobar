7d012bf3b868e312db8c7007e4063f453fb0cc51
spazma <spazma@gmail.com>
2025-07-23 14:42:27 +0200

    status bar

---
+++ status_bar.js
@@ -0,0 +1,6 @@
+$if(%ispaused%,Paused,Gra i buczy...) | 
+%bitrate% kbps | %samplerate% Hz | $caps(%channels%) | 
+%playback_time%[ / %length%] [$progress(%playback_time_seconds%,%length_seconds%,30,=,-)] 
+%codec% | %date% rok | %genre% | %filesize_natural% 
+
+> build.%_foobar2000_version%
